2f827d680535bdf65bd632bc0bd75a9b7680db15
Moiz47 <moiz1995@gmail.com>
2019-11-29 09:59:36 +0530

    lightining decoder

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+const axios = require('axios');
+const bodyParser = require('body-parser')
+const bolt = require('bolt11')
+ 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/front.html'));
+   
+});
+
+app.post('/', function(req, res){
+    var printText = req.body.t;
+    var decoded = bolt.decode(printText);
+    res.json({decode: decoded})
+})
+
+
+
+app.listen(3000, function(){
+    console.log("Server started")
+});
+
+
+
+
